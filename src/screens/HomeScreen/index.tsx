4b0816335645409844bdd107a96de702f4e1debb
Jerez S <>
2021-05-18 15:04:20 -0700

    Created ProductItem component and corresponding styles file

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,64 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {View, StyleSheet} from 'react-native';
+import ProductItem from '../../components/ProductItem';
 
 const HomeScreen = () => {
   return (
     <View style={styles.page}>
-      {/* Render Product Compoment */}
-      <View style={styles.root}>
-        <Image
-          style={styles.image}
-          source={{
-            uri:
-              'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleanarchitecture.jpg',
-          }}
-        />
-
-        <View style={styles.rightContainer}>
-          <Text style={styles.title} numberOfLines={3}>
-            Logitech MX Master 3 Advanced Wireless Mouse for Mac - Bluetooth/USB
-          </Text>
-          {/* Ratings */}
-          <View style={styles.ratingsContainer}>
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star-half-full"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star-o"
-              size={18}
-              color={'#e47911'}
-            />
-
-            <Text>13,032</Text>
-          </View>
-          <Text style={styles.price}>from $13.59
-          <Text style={styles.oldPrice}> $16.22</Text>
-          </Text>
-        </View>
-      </View>
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
+      <ProductItem />
     </View>
   );
 };
@@ -66,43 +17,6 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   page: {
     padding: 10,
-  },
-  root: {
-    flexDirection: 'row',
-    borderWidth: 1,
-    borderColor: '#d1d1d1',
-    borderRadius: 10,
-    backgroundColor: '#fff',
-  },
-  image: {
-    flex: 2,
-    height: 150,
-    margin: 8,
-    resizeMode: 'contain',
-  },
-  rightContainer: {
-    padding: 10,
-    flex: 3,
-  },
-  ratingsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  star: {
-    margin: 2,
-  },
-  title: {
-    fontSize: 18,
-  },
-  price: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  oldPrice: {
-    fontSize: 12,
-    fontWeight: 'normal',
-    textDecorationLine: 'line-through',
   },
 });
 
